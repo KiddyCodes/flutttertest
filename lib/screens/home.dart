3f6039a91f22542591f6487ab1f49b1c2f40bdc7
kiddyCodes <amadimarvellous1@gmail.com>
2023-09-03 13:04:07 -0700

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'HI',
+          style: TextStyle(fontSize: 40),
+        ),
+        Card()
+      ],
+    ));
+  }
+}
